fa8e25f347e4cfbc16fb9a22999e95c046557ea5
Özgürhan Polat <56520139+polat-code@users.noreply.github.com>
2022-08-21 13:46:32 +0300

    Continue

---
+++ src/test/java/com/example/springbootjpatutorial/repository/TeacherRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.springbootjpatutorial.repository;
+
+import com.example.springbootjpatutorial.entity.Course;
+import com.example.springbootjpatutorial.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Test
+    public void saveTeacher() {
+
+        Course courseDb = Course.builder()
+                .title("DBA")
+                .credit(5)
+                .build();
+        Course courseJava = Course.builder()
+                .title("Java")
+                .credit(6)
+                .build();
+
+        Teacher teacher =
+                Teacher.builder()
+                        .firstName("Özgürhan")
+                        .lastName("Polat")
+                        .courses(List.of(courseDb,courseJava))
+                        .build();
+    }
+}
